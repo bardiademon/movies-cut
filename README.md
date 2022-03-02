ab0eb33025a2f54b01e66958e0ae09e3265b127d
bardiademon <bardiademon@gmail.com>
2022-03-02 13:30:35 +0330

    Updated

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # movies-cut
 Divide the film into separate parts
+
+
+## How to use
+
+1) Download ffmpeg
+    ```
+    https://www.ffmpeg.org/download.html
+   ```
+   
+2) ffmpeg move to LIB_DIR
+    ```
+        ROOT/lib
+   ```
+   
+3) Run jar file 
+    ```
+        java[1.8 or higher] -jar ROOT/MovieCut.jar
+   ```
--- src/main/java/com/bardiademon/moviescut/Main.java
+++ src/main/java/com/bardiademon/moviescut/Main.java
@@ -1,10 +1,35 @@
 package com.bardiademon.moviescut;
 
+import javax.swing.UnsupportedLookAndFeelException;
+import java.util.Locale;
+
 public final class Main
 {
+    private static final String DEFAULT_LOOK_AND_FEEL_INFO = "windows";
+
     public static void main(final String[] args)
     {
         bardiademon.run();
+        configView();
         new MoviesCut();
+    }
+
+    private static void configView()
+    {
+        try
+        {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels())
+            {
+                if (DEFAULT_LOOK_AND_FEEL_INFO.equals(info.getName().toLowerCase(Locale.ROOT)))
+                {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        }
+        catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
     }
 }
